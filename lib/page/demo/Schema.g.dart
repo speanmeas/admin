57d439c785f062f51a9f574dea382dce68c25c54
MUY SENGLY <muy.sengly@itc.edu.kh>
2026-07-01 15:52:59 +0700

    update

--- lib/page/demo/Schema.g.dart
+++ lib/page/demo/Schema.g.dart
@@ -1,12 +1,62 @@
 List<Map<String, dynamic>> schema = [
-  {"key": "id", "type": "id", "title": "ID", "value": null},
-  {"key": "text_1", "type": "string", "title": "Text 1", "value": null},
-  {"key": "text_2", "type": "string", "title": "Text 2", "value": null},
-  {"key": "number_1", "type": "number", "title": "Number 1", "value": null},
-  {"key": "number_2", "type": "number", "title": "Number 2", "value": null},
-  {"key": "datetime_1", "type": "date-time", "title": "Datetime 1", "value": null},
-  {"key": "datetime_2", "type": "date-time", "title": "Datetime 2", "value": null},
-  {"key": "boolean_1", "type": "boolean", "title": "Boolean 1", "value": null},
-  {"key": "boolean_2", "type": "boolean", "title": "Boolean 2", "value": null},
-  {"key": "note", "type": "string", "title": "Note", "value": null},
+  {
+    "key": "id",
+    "type": "id",
+    "title": "ID",
+    "value": null
+  },
+  {
+    "key": "text_1",
+    "type": "string",
+    "title": "Text 1",
+    "value": null
+  },
+  {
+    "key": "text_2",
+    "type": "string",
+    "title": "Text 2",
+    "value": null
+  },
+  {
+    "key": "number_1",
+    "type": "number",
+    "title": "Number 1",
+    "value": null
+  },
+  {
+    "key": "number_2",
+    "type": "number",
+    "title": "Number 2",
+    "value": null
+  },
+  {
+    "key": "datetime_1",
+    "type": "date-time",
+    "title": "Datetime 1",
+    "value": null
+  },
+  {
+    "key": "datetime_2",
+    "type": "date-time",
+    "title": "Datetime 2",
+    "value": null
+  },
+  {
+    "key": "boolean_1",
+    "type": "boolean",
+    "title": "Boolean 1",
+    "value": null
+  },
+  {
+    "key": "boolean_2",
+    "type": "boolean",
+    "title": "Boolean 2",
+    "value": null
+  },
+  {
+    "key": "note",
+    "type": "string",
+    "title": "Note",
+    "value": null
+  }
 ];
