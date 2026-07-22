beddd169d926b9e44610db4bb6f02e4c306e1772
MUY SENGLY <muy.sengly@itc.edu.kh>
2026-07-22 22:36:21 +0700

    update

--- update_github.py
+++ update_github.py
@@ -1,4 +1,5 @@
 import os
+import re
 import time
 from tqdm import tqdm
 from rich import print
@@ -14,6 +15,42 @@ content_new = content_old.replace("bool is_github = false;", "bool is_github = t
 if content_new != content_old:
     with open("lib/__config__.dart", "w", encoding="utf-8") as f:
         f.write(content_new)
+
+
+#! read pubspec.yaml
+content = ""
+with open("pubspec.yaml", "r", encoding="utf-8") as f:
+    content = f.read()
+
+
+#! find the current build number
+build_match = re.search(r"version: (\d+).(\d+).(\d+)\+(\d+)", content)
+if build_match:
+
+    major = int(build_match.group(1))
+    # print(f"major : {major}")
+
+    minor = int(build_match.group(2))
+    # print(f"minor : {minor}")
+
+    patch = int(build_match.group(3))
+    # print(f"patch : {patch}")
+
+    old_build_num = int(build_match.group(4))
+    new_build_num = old_build_num + 1
+    # print(f"build_num : {build_num} -> new_build_num : {new_build_num}")
+
+    # update the build number in pubspec.yaml content
+    new_content = re.sub(
+        r"version: (\d+)\.(\d+)\.(\d+)\+(\d+)",
+        f"version: {build_match.group(1)}.{build_match.group(2)}.{build_match.group(3)}+{new_build_num}",
+        content,
+    )
+    # print(new_content)
+
+    # write back to env.dart
+    with open("pubspec.yaml", "w", encoding="utf-8") as f:
+        f.write(new_content)
 
 
 #! clean
@@ -39,4 +76,4 @@ with open("lib/__config__.dart", "w", encoding="utf-8") as f:
     f.write(content_old)
 
 
-print(f"Built and pushed to GitHub successfully!")
+print(f"Built: {old_build_num} -> {new_build_num}")
