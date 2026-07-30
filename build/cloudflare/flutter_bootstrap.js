a3d2fd645963bade9b4d6e9e1d57a09b624e1893
MUY SENGLY <muy.sengly@itc.edu.kh>
2026-07-30 13:06:29 +0700

    update

--- build/cloudflare/flutter_bootstrap.js
+++ build/cloudflare/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1737519655" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3925571490" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
