b5c90e2428c4457c0d5db22fcc25b181caa6318e
MUY SENGLY <muysengly@gmail.com>
2026-09-03 18:48:42 +0700

    2026-09-03 18:48:41

--- build/cloudflare/flutter_bootstrap.js
+++ build/cloudflare/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1377470076" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1370150794" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
